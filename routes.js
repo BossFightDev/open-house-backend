2c6f4bf6edbbf380453740114983dc1cf8110fce
WizrdJim <wzrdjim@gmail.com>
2017-12-13 14:41:11 -0500

    added cloudinary photo upload to the api

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cloudinary = require('cloudinary')
+const multer = require('multer')
 let config
 !process.env.CNAME? config = require('./config') : null
 const { Property, Lead, OpenHouse, User } = require('./models')
@@ -7,11 +8,13 @@ const cloud_name = process.env.CNAME || config.cloudName
 const api_key = process.env.APIKEY || config.apiKey
 const api_secret = process.env.APISECRET || config.apiSecret
 
-cloudinary.config({
-  cloud_name,
-  api_key,
-  api_secret,
-})
+// cloudinary.config({
+//   cloud_name,
+//   api_key,
+//   api_secret,
+// })
+
+const upload = multer({ dest : './public/uploads'}).single('userPhoto')
 
 const router = express.Router()
 
@@ -21,10 +24,36 @@ router.get('/listings', (req, res) => {
 router.post('/user', (req, res) => {
   const {firstname, lastname, username, password, phonenumber, company } = req.body
 })
-router.post('/picture', (req, res)=> {
-  cloudinary.uploader.upload(req.body.file, (result) => {
-    console.log(result);
-  })
+router.post('/photo', function(req,res){
+	upload(req, res, function(err){		
+		if(err){ return res.end("Error")};
+		console.log(req);
+		res.end("file uploaded")
+
+		cloudinary.config({ 
+	      cloud_name, 
+	      api_key, 
+	      api_secret
+	    })
+
+    cloudinary.uploader.upload(req.file.path, function(result) { 
+      console.log(result);
+        // //create an urembo product
+        // var photo = new Photo();
+        //   photo.name = req.body.name;
+        //   photo.picture = result.url;
+        //   photo.place = req.body.place;
+        //   photo.city = req.body.city;
+        // //save the product and check for errors
+        // photo.save(function(err, photos){
+        //   if(err) 
+        //     res.send(err);
+        //   res.json({ message: 'photographed place created.'});
+        //   console.log(photos);
+        // });
+    })
+
+   })	
 })
 
 module.exports = router
